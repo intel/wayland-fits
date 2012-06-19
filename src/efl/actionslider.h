6c3c526f76e787ca38344810e40590d14d3f2945
Joe Konno <joe.konno@intel.com>
2012-06-19 10:37:36 -0700

    Add actionslider widget object
    Signed-off-by: Joe Konno <joe.konno@intel.com>

---
+++ src/efl/actionslider.h
@@ -0,0 +1,25 @@
+#ifndef __WAYLAND_EFL_ACTIONSLIDER_H__
+#define __WAYLAND_EFL_ACTIONSLIDER_H__
+
+#include <string>
+#include <Elementary.h>
+
+#include "evasobject.h"
+
+class Actionslider : public EvasObject
+{
+public:
+	Actionslider(Evas_Object *, const std::string& = "Left", const bool = true, const std::string& = "Center", const bool = true, const std::string& = "Right", const bool = true);
+
+	void setPos(Elm_Actionslider_Pos pos);
+
+	Elm_Actionslider_Pos getPos();
+
+	operator Evas_Object*();
+
+private:
+	Evas_Object *parent;
+};
+
+#endif
+
