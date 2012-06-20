90285dd54599b5b12a749726efb9292a7510b605
Joe Konno <joe.konno@intel.com>
2012-06-20 10:59:31 -0700

    background: wrap image setting
    Signed-off-by: Joe Konno <joe.konno@intel.com>

--- src/efl/background.cpp
+++ src/efl/background.cpp
@@ -14,9 +14,23 @@ void Background::setColor(int red, int green, int blue)
 	elm_bg_color_set(*this, red, green, blue);
 }
 
+void Background::setImage(const path& p)
+{
+	(void) elm_bg_file_set(*this, p.c_str(), NULL);
+}
+
 void Background::getColor(int* red, int* green, int* blue)
 {
 	elm_bg_color_get(*this, red, green, blue);
+}
+
+void Background::getImage(path& p)
+{
+	char* ret = NULL;
+
+	elm_bg_file_get(*this, const_cast<const char**>(&ret), NULL);
+
+	p = ret;
 }
 
 Background::operator Evas_Object*()
--- src/efl/background.h
+++ src/efl/background.h
@@ -1,10 +1,16 @@
 #ifndef __WAYLAND_EFL_BACKGROUND_H__
 #define __WAYLAND_EFL_BACKGROUND_H__
 
-#include <string>
-#include <Elementary.h>
+#ifndef MEDIA_PATH
+#error "The MEDIA_PATH macro must be defined-- pass definition in CXXFLAGS."
+#endif
 
+#define BOOST_FILESYSTEM_VERSION 3
+#include <boost/filesystem.hpp>
+#include <Elementary.h>
 #include "evasobject.h"
+
+using boost::filesystem::path;
 
 class Background : public EvasObject
 {
@@ -12,8 +18,10 @@ public:
 	Background(Evas_Object *parent);
 
 	void setColor(int, int, int);
+	void setImage(const path&);
 
 	void getColor(int*, int*, int*);
+	void getImage(path&);
 
 	operator Evas_Object*();
 
