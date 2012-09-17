327429002d9712a382a173787dfd5f3ef9d9fcd8
U. Artie Eoff <ullysses.a.eoff@intel.com>
2012-09-17 14:11:10 -0700

    core: add cursor theme tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- src/core/test_bind_interface.cpp
+++ src/core/test_bind_interface.cpp
@@ -3,25 +3,34 @@
 #include "common/test.h"
 
 template <typename O, const wl_interface& interface, const std::string& str_interface>
-struct BindInterface {
+class BindInterface
+{
+public:
 	BindInterface()
-		: display(wl_display_connect(0))
-		, object(0)
+		: display_(wl_display_connect(0))
+		, object_(0)
+		, listener_(0)
 	{
-		FAIL_IF_EQUAL(display, NULL);
+		FAIL_IF_EQUAL(display_, NULL);
 	}
-	
+
+	~BindInterface()
+	{
+		wl_display_remove_global_listener(display_, listener_);
+		wl_display_disconnect(display_);
+	}
+
 	void bind()
 	{
-		wl_display_add_global_listener(display, &BindInterface::callback, this);
-		wl_display_iterate(display, WL_DISPLAY_READABLE);
+		listener_ = wl_display_add_global_listener(display_, &BindInterface::callback, this);
+		wl_display_iterate(display_, WL_DISPLAY_READABLE);
 	}
 
 	static void callback(wl_display* display, uint32_t id, const char* iface, uint32_t version, void* data)
 	{
 		BindInterface* bi = static_cast<BindInterface*>(data);
 		if (std::string(iface) == str_interface) {
-				bi->object = static_cast<O*>(
+				bi->object_ = static_cast<O*>(
 					wl_display_bind(
 						display, id, &interface
 					)
@@ -29,8 +38,9 @@ struct BindInterface {
 			}
 	}
 
-	wl_display*	display;
-	O*		object;
+	wl_display*			display_;
+	O*					object_;
+	wl_global_listener*	listener_;
 };
 
 #define BIND_TEST(name) \
@@ -39,9 +49,9 @@ struct BindInterface {
 	TEST(bind_##name, "Core/BindInterface") \
 	{ \
 		bind_##name##_interface bi; \
-		FAIL_UNLESS(bi.object == NULL); \
+		FAIL_UNLESS(bi.object_ == NULL); \
 		bi.bind(); \
-		FAIL_IF(bi.object == NULL); \
+		FAIL_IF(bi.object_ == NULL); \
 	}
 
 BIND_TEST(wl_compositor)
