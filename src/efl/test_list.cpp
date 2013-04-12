1d7dbbca1ecec2ddd1616ce5e55feda90ae2f137
U. Artie Eoff <ullysses.a.eoff@intel.com>
2013-04-12 16:28:57 -0700

    efl test_list: cleanup code and minor refactoring.
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- src/efl/test_list.cpp
+++ src/efl/test_list.cpp
@@ -1,13 +1,11 @@
-#include <vector>
 #include <string>
-
+#include <linux/input.h>
 #include <boost/lexical_cast.hpp>
 #include <boost/concept_check.hpp>
+
 #include "window.h"
 #include "elmtestharness.h"
 #include "templates.h"
-#include <vector>
-#include <linux/input.h>
 
 class List : public EvasObject
 {
@@ -20,10 +18,10 @@ public:
 };
 
 //TODO: Add mouse wheel tests
-class UserListTestHarness : public ElmTestHarness
+class ListTestHarness : public ElmTestHarness
 {
 public:
-	UserListTestHarness()
+	ListTestHarness()
 		: ElmTestHarness::ElmTestHarness()
 		, window_("UserListInteractionTest", "User List Interaction Test")
 		, list_(elm_list_add(window_))
@@ -37,11 +35,10 @@ public:
 		, activated_(false)
 		, doubleClicked_(false)
 		, longPressed_(false)
-		, centerOfWindow_(std::pair<int, int>(0,0))
 	{
-		for (int i(0); i <= 13; i++) {
+		for (unsigned i(0); i <= 13; ++i) {
 			const std::string si = boost::lexical_cast<std::string>(i);
-			elm_list_item_append(list_, si.c_str(), NULL, NULL, itemSelectCallback, this); //last pointer is called "Input handle pointer"
+			elm_list_item_append(list_, si.c_str(), NULL, NULL, onSelected, this);
 		}
 
 		evas_object_size_hint_weight_set(list_, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
@@ -56,7 +53,6 @@ public:
 		evas_object_smart_callback_add(list_, "clicked,double", onDoubleClick, this);
 		evas_object_smart_callback_add(list_, "activated", onActivated, this);
 		evas_object_smart_callback_add(list_, "longpressed", onLongpressed, this);
-
 	}
 
 	void setup()
@@ -67,7 +63,6 @@ public:
 		window_.show();
 
 		Application::yield(0.01*1e6);
-
 		Geometry geo = getSurfaceGeometry(window_.get_wl_surface());
 		Geometry geoListItem;
 		Geometry geoFS = window_.getFramespaceGeometry();
@@ -75,9 +70,6 @@ public:
 		Elm_Object_Item *it = elm_list_first_item_get(list_);
 		Evas_Object *eo = elm_list_item_object_get(it);
 		evas_object_geometry_get(eo, &geoListItem.x, &geoListItem.y, &geoListItem.width, &geoListItem.height);
-
-		centerOfWindow_.first = geo.x + window_.getWidth() / 2;
-		centerOfWindow_.second = geo.y + window_.getHeight() / 2;
 
 		//Place pointer at center of top item of list
 		queueStep(
@@ -88,76 +80,78 @@ public:
 			)
 		);
 
-		queueStep(boost::bind(&UserListTestHarness::runTest, boost::ref(*this)));
+		queueStep(boost::bind(&ListTestHarness::runTest, boost::ref(*this)));
 	}
 
 	virtual void runTest() = 0;
 
-	static void itemSelectCallback(void* data, Evas_Object* li, void*)
+	static void onSelected(void* data, Evas_Object* li, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
-
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		Elm_Object_Item *lit = elm_list_selected_item_get(li);
-		std::cout << "...received itemSelectCallback for item: " << elm_object_item_text_get(lit) << std::endl;
-		
-		if(not strcmp(elm_object_item_text_get(lit), "0"))
+		const std::string strItem(elm_object_item_text_get(lit));
+
+		std::cout << "...received selected event: " << strItem << std::endl;
+
+		if (strItem == "0") {
 			test->firstItemSelected_ = true;
-		if(not strcmp(elm_object_item_text_get(lit), "1"))
+		} else if (strItem == "1") {
 			test->secondItemSelected_ = true;
+		}
 	}
 
 	static void onUnselected(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->unselected_ = true;
 		std::cout << "...received unselected event" << std::endl;
 	}
 
 	static void onEdgeTop(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->edgeTop_ = true;
 		std::cout << "...received edge,top event" << std::endl;
 	}
 
 	static void onEdgeBottom(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->edgeBottom_ = true;
 		std::cout << "...received edge,bottom event" << std::endl;
 	}
 
 	static void onEdgeLeft(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->edgeLeft_ = true;
 		std::cout << "...received edge,left event" << std::endl;
 	}
 
 	static void onEdgeRight(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->edgeRight_ = true;
 		std::cout << "...received edge,right event" << std::endl;
 	}
 
 	static void onDoubleClick(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->doubleClicked_ = true;
 		std::cout << "...received clicked,double event" << std::endl;
 	}
 
 	static void onActivated(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->activated_ = true;
 		std::cout << "...received activated event" << std::endl;
 	}
 
 	static void onLongpressed(void* data, Evas_Object*, void*)
 	{
-		UserListTestHarness *test = static_cast<UserListTestHarness*>(data);
+		ListTestHarness *test = static_cast<ListTestHarness*>(data);
 		test->longPressed_= true;
 		std::cout << "...received longpressed event" << std::endl;
 	}
@@ -176,11 +170,9 @@ protected:
 	bool		activated_;
 	bool		doubleClicked_;
 	bool		longPressed_;
-
-	std::pair<int, int> centerOfWindow_;
 };
 
-class UserListSelectCase : public UserListTestHarness
+class ListUserSelectTest : public ListTestHarness
 {
 public:
 	void runTest()
@@ -189,14 +181,14 @@ public:
 		inputKeySend(BTN_LEFT, 1);
 		inputKeySend(BTN_LEFT, 0);
 		std::cout << "...checking for firstSelected callback" << std::endl;
-		while(not firstItemSelected_){
+		while (not firstItemSelected_) {
 			Application::yield();
 		}
 
 		std::cout << "...checking for unselected and secondSelected callback" << std::endl;
 		ASSERT(not unselected_);
 		ASSERT(not secondItemSelected_);
-		while(not unselected_ and not secondItemSelected_){
+		while (not unselected_ and not secondItemSelected_) {
 			inputKeySend(KEY_DOWN, 1);
 			inputKeySend(KEY_DOWN, 0);
 			Application::yield();
@@ -204,14 +196,14 @@ public:
 	}
 };
 
-class UserListLongpressCase : public UserListTestHarness
+class ListUserLongpressTest : public ListTestHarness
 {
 public:
 	void runTest()
 	{
 		ASSERT(longPressed_ == false);
 		inputKeySend(BTN_LEFT, 1);
-		//TODO: Investigate time and sleeping for longpress
+
 		std::cout << "...checking for longpressed callback" << std::endl;
 		while (not longPressed_) {
 			Application::yield();
@@ -220,7 +212,7 @@ public:
 	}
 };
 
-class UserListActivatedTest : public UserListTestHarness
+class ListUserActivatedTest : public ListTestHarness
 {
 public:
 	void runTest()
@@ -230,14 +222,15 @@ public:
 		inputKeySend(BTN_LEFT, 0);
 		inputKeySend(KEY_ENTER, 1);
 		inputKeySend(KEY_ENTER, 0);
+
 		std::cout << "...checking for activated callback" << std::endl;
-		while(not activated_){
+		while (not activated_) {
 			Application::yield();
 		}
 	}
 };
 
-class UserListDoubleClickTest : public UserListTestHarness
+class ListUserDoubleClickTest : public ListTestHarness
 {
 public:
 	void runTest()
@@ -247,24 +240,31 @@ public:
 		inputKeySend(BTN_LEFT, 0);
 		inputKeySend(BTN_LEFT, 1);
 		inputKeySend(BTN_LEFT, 0);
+
 		std::cout << "...checking for clicked,double callback" << std::endl;
-		while(not doubleClicked_){
+		while (not doubleClicked_) {
 			Application::yield();
 		}
 	}
 };
 
-class UserListKeyScrollTest : public UserListTestHarness
+class ListUserKeyScrollTest : public ListTestHarness
 {
 public:
 	void runTest()
 	{
+		/*
+		 * FIXME: On some systems, it doesn't appear that the scroller
+		 * bounce animation works.  This test is strictly dependent on
+		 * that feature and thus should be refactored so it's not
+		 * dependent on it.
+		 */
 		inputKeySend(BTN_LEFT, 1);
 		inputKeySend(BTN_LEFT, 0);
 
 		std::cout << "...checking for edge,top callback" << std::endl;
 		ASSERT(not edgeTop_);
-		while(not edgeTop_){
+		while (not edgeTop_) {
 			inputKeySend(KEY_UP, 1);
 			inputKeySend(KEY_UP, 0);
 			Application::yield();
@@ -272,7 +272,7 @@ public:
 
 		std::cout << "...checking for edge,bottom callback" << std::endl;
 		ASSERT(not edgeBottom_);
-		while(not edgeBottom_){
+		while (not edgeBottom_) {
 			inputKeySend(KEY_DOWN, 1);
 			inputKeySend(KEY_DOWN, 0);
 			Application::yield();
@@ -282,7 +282,7 @@ public:
 
 		std::cout << "...checking for edge,left callback" << std::endl;
 		ASSERT(not edgeLeft_);
-		while(not edgeLeft_){
+		while (not edgeLeft_) {
 			inputKeySend(KEY_LEFT, 1);
 			inputKeySend(KEY_LEFT, 0);
 			Application::yield();
@@ -290,7 +290,7 @@ public:
 
 		std::cout << "...checking for edge,right callback" << std::endl;
 		ASSERT(not edgeRight_);
-		while(not edgeRight_){
+		while (not edgeRight_) {
 			inputKeySend(KEY_RIGHT, 1);
 			inputKeySend(KEY_RIGHT, 0);
 			Application::yield();
@@ -300,31 +300,45 @@ public:
 	}
 };
 
-class UserListMouseScrollTest : public UserListTestHarness
+class ListUserMouseScrollTest : public ListTestHarness
 {
 public:
 	void runTest()
 	{
+		/*
+		 * FIXME: On some systems, it doesn't appear that you can
+		 * scroll the list with a mouse left-button down and move
+		 * action.  Investigate why to determine if it's an actual
+		 * system-related thing or some toolkit config is missing.
+		 * If it's system-dependent, then we don't want this test.
+		 * Also, appears to depend on the bounce animation which
+		 * may not be available on some setups.
+		 */
 		int delta(40);
 
+		Position center;
+		Geometry geo = getSurfaceGeometry(window_.get_wl_surface());
+		center.x = geo.x + window_.getWidth() / 2;
+		center.y = geo.y + window_.getHeight() / 2;
+
 		std::cout << "...checking for edge,top callback" << std::endl;
-		setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second);
+		setGlobalPointerPosition(center);
 		ASSERT(not edgeTop_);
-		while(not edgeTop_){
+		while (not edgeTop_) {
 			inputKeySend(BTN_LEFT, 1);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second + delta);
+			setGlobalPointerPosition(center.x, center.y + delta);
 			inputKeySend(BTN_LEFT, 0);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second);
+			setGlobalPointerPosition(center);
 			Application::yield(500000); //animation has to be displayed before callback is made :-/
 		}
 
 		std::cout << "...checking for edge,bottom callback" << std::endl;
 		ASSERT(not edgeBottom_);
-		while(not edgeBottom_){
+		while (not edgeBottom_) {
 			inputKeySend(BTN_LEFT, 1);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second - delta);
+			setGlobalPointerPosition(center.x, center.y - delta);
 			inputKeySend(BTN_LEFT, 0);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second);
+			setGlobalPointerPosition(center);
 			Application::yield(500000);
 		}
 
@@ -332,21 +346,21 @@ public:
 
 		std::cout << "...checking for edge,left callback" << std::endl;
 		ASSERT(not edgeLeft_);
-		while(not edgeLeft_){
+		while (not edgeLeft_) {
 			inputKeySend(BTN_LEFT, 1);
-			setGlobalPointerPosition(centerOfWindow_.first + delta, centerOfWindow_.second);
+			setGlobalPointerPosition(center.x + delta, center.y);
 			inputKeySend(BTN_LEFT, 0);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second);
+			setGlobalPointerPosition(center);
 			Application::yield(500000);
 		}
 
 		std::cout << "...checking for edge,right callback" << std::endl;
 		ASSERT(not edgeRight_);
-		while(not edgeRight_){
+		while (not edgeRight_) {
 			inputKeySend(BTN_LEFT, 1);
-			setGlobalPointerPosition(centerOfWindow_.first - delta, centerOfWindow_.second);
+			setGlobalPointerPosition(center.x - delta, center.y);
 			inputKeySend(BTN_LEFT, 0);
-			setGlobalPointerPosition(centerOfWindow_.first, centerOfWindow_.second);
+			setGlobalPointerPosition(center);
 			Application::yield(500000);
 		}
 
@@ -359,13 +373,13 @@ typedef ResizeObjectTest<List> ListResizeTest;
 typedef PositionObjectTest<List> ListPositionTest;
 typedef VisibleObjectTest<List> ListVisibilityTest;
 
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListSelectCase, "List")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListLongpressCase, "List")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListActivatedTest, "List")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListDoubleClickTest, "List")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListKeyScrollTest, "List")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserListMouseScrollTest, "List")
 WAYLAND_ELM_HARNESS_TEST_CASE(ListResizeTest, "List")
 WAYLAND_ELM_HARNESS_TEST_CASE(ListPositionTest, "List")
 WAYLAND_ELM_HARNESS_TEST_CASE(ListVisibilityTest, "List")
 
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserSelectTest, "List")
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserLongpressTest, "List")
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserActivatedTest, "List")
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserDoubleClickTest, "List")
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserKeyScrollTest, "List")
+WAYLAND_ELM_HARNESS_TEST_CASE(ListUserMouseScrollTest, "List")
