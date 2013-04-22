cf429e730016d10efb9609c7042d287d6d0e8c09
Brian Lovin <brian.j.lovin@intel.com>
2013-04-22 14:05:40 -0700

    test_check: Added user interaction test.
    Signed-off-by: Brian Lovin <brian.j.lovin@intel.com>

--- src/efl/test_check.cpp
+++ src/efl/test_check.cpp
@@ -1,8 +1,7 @@
-#include <Elementary.h>
-#include <boost/bind.hpp>
+#include <string>
+#include <linux/input.h>
 
 #include "window.h"
-
 #include "elmtestharness.h"
 #include "templates.h"
 
@@ -84,6 +83,108 @@ private:
 	EvasObject	check_;
 };
 
+class CheckUserStateTest : public ElmTestHarness
+{
+public:
+
+	CheckUserStateTest()
+		: ElmTestHarness::ElmTestHarness()
+		, window_("CheckUserStateTest", "Check User State Test")
+		, check_(elm_check_add(window_))
+		, changed_(false)
+	{
+		evas_object_resize(check_, 200, 100);
+		elm_object_text_set(check_, "Test Check");
+		evas_object_move(check_, 0, 0);
+		evas_object_smart_callback_add(check_, "changed", onChanged, this);
+	}
+
+	void setup()
+	{
+		check_.show();
+		window_.show();
+
+		queueStep(boost::bind(&CheckUserStateTest::clickCheck, boost::ref(*this)));
+		queueStep(
+			boost::bind(
+				&ElmTestHarness::stepUntilCondition,
+				boost::ref(*this),
+				boost::lambda::bind(&CheckUserStateTest::changed_, boost::ref(*this))
+			),
+			"Testing changed_ event"
+		);
+		queueStep(
+			boost::bind(
+				&CheckUserStateTest::testCheckState,
+				boost::ref(*this),
+				EINA_TRUE
+			),
+			"Testing state == EINA_TRUE"
+		);
+
+		queueStep(boost::bind(&CheckUserStateTest::setClickedFalse, boost::ref(*this)));
+		queueStep(boost::bind(&CheckUserStateTest::clickCheck, boost::ref(*this)));
+		queueStep(
+			boost::bind(
+				&ElmTestHarness::stepUntilCondition,
+				boost::ref(*this),
+				boost::lambda::bind(&CheckUserStateTest::changed_, boost::ref(*this))
+			),
+			"Testing changed_ event"
+		);
+		queueStep(
+			boost::bind(
+				&CheckUserStateTest::testCheckState,
+				boost::ref(*this),
+				EINA_FALSE
+			),
+			"Testing state == EINA_FALSE"
+		);
+	}
+
+	void clickCheck()
+	{
+		Application::yield(0.01*1e6);
+
+		Geometry gw(getSurfaceGeometry(window_.get_wl_surface()));
+		Geometry gf(window_.getFramespaceGeometry());
+		Geometry gb(check_.getGeometry());
+
+		setGlobalPointerPosition(
+			gw.x + gf.x + gb.x + gb.width / 2,
+			gw.y + gf.y + gb.y + gb.height / 2
+		);
+
+		ASSERT(changed_ == false);
+
+		inputKeySend(BTN_LEFT, 1);
+		inputKeySend(BTN_LEFT, 0);
+	}
+
+	static void onChanged(void* data, Evas_Object*, void*)
+	{
+		CheckUserStateTest *test = static_cast<CheckUserStateTest*>(data);
+		test->changed_ = true;
+		std::cout << "...received changed event" << std::endl;
+	}
+
+	void testCheckState(Eina_Bool checked)
+	{
+		FAIL_UNLESS_EQUAL(elm_check_state_get(check_), checked);
+	}
+
+	void setClickedFalse(){
+		changed_ = false;
+	}
+
+private:
+	Window		window_;
+	EvasObject	check_;
+
+	bool		changed_;
+};
+
+
 std::string defaultPart("default"), defaultText("DEFAULT");
 
 
@@ -97,6 +198,8 @@ WAYLAND_ELM_HARNESS_TEST_CASE(CheckPositionTest, "Check")
 WAYLAND_ELM_HARNESS_TEST_CASE(CheckVisibilityTest, "Check")
 WAYLAND_ELM_HARNESS_TEST_CASE(CheckStateTest, "Check")
 WAYLAND_ELM_HARNESS_TEST_CASE(CheckPartTextDefaultTest, "Check")
+
+WAYLAND_ELM_HARNESS_TEST_CASE(CheckUserStateTest, "Check")
 
 // NOTE: The following tests fail on all engines:
 // see http://trac.enlightenment.org/e/ticket/2285
