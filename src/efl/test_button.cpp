95b5955e5a4e5cc745308b0acf6d42b1f87738ba
U. Artie Eoff <ullysses.a.eoff@intel.com>
2013-04-26 18:12:27 -0700

    efl: fix and refactor button user tests
    EFL 8235272ec053eb4bfcb040d7c9c20b61c55ffb61 enabled async
    shm rendering.  Thus, we need to wait for first post render
    event before expecting that a window's surface has been mapped
    on server-side.
    
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- src/efl/test_button.cpp
+++ src/efl/test_button.cpp
@@ -17,329 +17,302 @@ public:
 	}
 };
 
-class UserButtonInteractionTest : public ElmTestHarness
+class ButtonUserMouseClickTest : public ElmTestHarness
 {
 public:
-	UserButtonInteractionTest()
+	ButtonUserMouseClickTest()
 		: ElmTestHarness::ElmTestHarness()
-		, window_("UserButtonClickTest", "User Button Click Test")
+		, window_("ButtonUserMouseClickTest", "Button User Mouse Click Test")
 		, button_(elm_button_add(window_))
 		, clicked_(false)
 		, repeated_(false)
 		, pressed_(false)
 		, unpressed_(false)
+		, rendered_(false)
 	{
-		evas_object_resize(button_, 200, 100);
-		elm_object_text_set(button_, "Test Button!");
-		evas_object_smart_callback_add(button_, "clicked", onClick, this);
-		evas_object_smart_callback_add(button_, "repeated", onRepeated, this);
-		evas_object_smart_callback_add(button_, "pressed", onPressed, this);
-		evas_object_smart_callback_add(button_, "unpressed", onUnpressed, this);
-		
-		elm_button_autorepeat_set(button_, EINA_TRUE);
-		elm_button_autorepeat_initial_timeout_set(button_, 1.0);
-		elm_button_autorepeat_gap_timeout_set(button_, 0.5);
+		return;
 	}
 
 	void setup()
 	{
-		Geometry geoFS = window_.getFramespaceGeometry();
+		evas_object_resize(button_, 200, 100);
+		elm_object_text_set(button_, "Test Button!");
+
 		//place button in the center of window
 		button_.setPosition(
 			(window_.getWidth() / 2 - button_.getWidth() / 2),
 			(window_.getHeight() / 2 - button_.getHeight() / 2)
 		);
+
+		elm_button_autorepeat_set(button_, EINA_TRUE);
+		elm_button_autorepeat_initial_timeout_set(button_, 1.0);
+		elm_button_autorepeat_gap_timeout_set(button_, 0.5);
+
+		evas_event_callback_add(
+			evas_object_evas_get(window_),
+			EVAS_CALLBACK_RENDER_POST,
+			onPostRender, this
+		);
+
+		evas_object_smart_callback_add(button_, "clicked", onClick, this);
+		evas_object_smart_callback_add(button_, "repeated", onRepeated, this);
+		evas_object_smart_callback_add(button_, "pressed", onPressed, this);
+		evas_object_smart_callback_add(button_, "unpressed", onUnpressed, this);
+
 		button_.show();
 		window_.show();
 
-		Application::yield(0.01*1e6);
-		Geometry geo = getSurfaceGeometry(window_.get_wl_surface());
+		queueStep(boost::bind(&ButtonUserMouseClickTest::test, boost::ref(*this)));
+	}
+
+	void test()
+	{
+		YIELD_UNTIL(rendered_);
+
+		Geometry framespace(window_.getFramespaceGeometry());
+		Geometry geometry(getSurfaceGeometry(window_.get_wl_surface()));
 
 		//Place pointer in center of button
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::setGlobalPointerPosition, boost::ref(*this),
-				(geo.x + button_.getX() + button_.getWidth() / 2),
-				(geo.y + button_.getY() + button_.getHeight() / 2 + geoFS.y)
-				)
+		setGlobalPointerPosition(
+			geometry.x + button_.getX() + button_.getWidth() / 2,
+			geometry.y + button_.getY() + button_.getHeight() / 2 + framespace.y
 		);
-		
-		//below are all callbacks this widget makes that the user can invoke
-		addEventsTest();
-		
+
+		ASSERT(not pressed_);
+		ASSERT(not repeated_);
+
+		inputKeySend(BTN_LEFT, 1);
+
+		YIELD_UNTIL(pressed_ and repeated_)
+
+		ASSERT(not unpressed_);
+		ASSERT(not clicked_);
+
+		inputKeySend(BTN_LEFT, 0);
+
+		YIELD_UNTIL(unpressed_ and clicked_)
 	}
-	
-	void addEventsTest()
+
+	static void onPostRender(void *data, Evas *e, void *info)
 	{
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::inputKeySend,
-				boost::ref(*this), BTN_LEFT, 1
-			),
-			"sending button left click down"
-		);
-		queueStep(
-			boost::bind(
-			&ElmTestHarness::stepUntilCondition,
-			boost::ref(*this),
-			boost::lambda::bind(&UserButtonInteractionTest::pressed_, boost::ref(*this))
-			),
-			"checking pressed event"
-		);
-		queueStep(
-			boost::bind(
-			&ElmTestHarness::stepUntilCondition,
-			boost::ref(*this),
-			boost::lambda::bind(&UserButtonInteractionTest::repeated_, boost::ref(*this))
-			),
-			"checking repeated event"
-		);
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::inputKeySend,
-				boost::ref(*this), BTN_LEFT, 0
-			),
-			"sending button left click up"
-		);
-		queueStep(
-			boost::bind(
-			&ElmTestHarness::stepUntilCondition,
-			boost::ref(*this),
-			boost::lambda::bind(&UserButtonInteractionTest::unpressed_, boost::ref(*this))
-			),
-			"checking unpressed event"
-		);
-		queueStep(
-			boost::bind(
-			&ElmTestHarness::stepUntilCondition,
-			boost::ref(*this),
-			boost::lambda::bind(&UserButtonInteractionTest::clicked_, boost::ref(*this))
-			),
-			"checking clicked event"
-		);
+		evas_event_callback_del(e, EVAS_CALLBACK_RENDER_POST, onPostRender);
+
+		ButtonUserMouseClickTest *test = static_cast<ButtonUserMouseClickTest*>(data);
+		test->rendered_ = true;
+		std::cout << "...received post render event" << std::endl;
 	}
 
 	static void onClick(void* data, Evas_Object*, void*)
 	{
-		UserButtonInteractionTest *test = static_cast<UserButtonInteractionTest*>(data);
+		ButtonUserMouseClickTest *test = static_cast<ButtonUserMouseClickTest*>(data);
 		test->clicked_ = true;
 		std::cout << "...received click event" << std::endl;
 	}
 	
 	static void onRepeated(void* data, Evas_Object*, void*)
 	{
-		UserButtonInteractionTest *test = static_cast<UserButtonInteractionTest*>(data);
+		ButtonUserMouseClickTest *test = static_cast<ButtonUserMouseClickTest*>(data);
 		test->repeated_ = true;
 		std::cout << "...received repeated event" << std::endl;
 	}
 	
 	static void onPressed(void* data, Evas_Object*, void*)
 	{
-		UserButtonInteractionTest *test = static_cast<UserButtonInteractionTest*>(data);
+		ButtonUserMouseClickTest *test = static_cast<ButtonUserMouseClickTest*>(data);
 		test->pressed_ = true;
-		std::cout << "...received pressed_ event" << std::endl;
+		std::cout << "...received pressed event" << std::endl;
 	}
 	
 	static void onUnpressed(void* data, Evas_Object*, void*)
 	{
-		UserButtonInteractionTest *test = static_cast<UserButtonInteractionTest*>(data);
+		ButtonUserMouseClickTest *test = static_cast<ButtonUserMouseClickTest*>(data);
 		test->unpressed_ = true;
-		std::cout << "...received unpressed_ event" << std::endl;
+		std::cout << "...received unpressed event" << std::endl;
 	}
 	
 private:
+	Window		window_;
+	EvasObject	button_;
 	bool		clicked_;
 	bool		repeated_;
 	bool		pressed_;
 	bool		unpressed_;
-	
-	Window		window_;
-	EvasObject	button_;
+	bool		rendered_;
 };
 
 
-class UserButtonEventTest : public ElmTestHarness
+class ButtonUserMouseInOutTest : public ElmTestHarness
 {
 public:
 	
-	UserButtonEventTest()
+	ButtonUserMouseInOutTest()
 		: ElmTestHarness::ElmTestHarness()
-		, window_("UserButtonEventTest", "Button Pointer Event Test")
+		, window_("ButtonUserMouseInOutTest", "Button Mouse In/Out Test")
 		, button_(elm_button_add(window_))
-		, isMouseInButton_(false)
-		, isMouseOutButton_(false)
+		, mouseIn_(false)
+		, rendered_(false)
 	{
-		evas_object_resize(button_, 200, 100);
-		elm_object_text_set(button_, "Test Button!");
-		evas_object_event_callback_add(button_, EVAS_CALLBACK_MOUSE_IN, onMouseInButton, this);
-		evas_object_event_callback_add(button_, EVAS_CALLBACK_MOUSE_OUT, onMouseOutButton, this);
+		return;
 	}
 
 	void setup()
 	{
+		evas_object_resize(button_, 200, 100);
+		elm_object_text_set(button_, "Test Button!");
 		button_.setPosition(0, 0);
+
+		evas_event_callback_add(
+			evas_object_evas_get(window_),
+			EVAS_CALLBACK_RENDER_POST,
+			onPostRender, this
+		);
+
+		evas_object_event_callback_add(button_, EVAS_CALLBACK_MOUSE_IN, onMouseIn, this);
+		evas_object_event_callback_add(button_, EVAS_CALLBACK_MOUSE_OUT, onMouseOut, this);
+
 		button_.show();
 		window_.show();
-		
-		Application::yield(0.01*1e6);
 
-		Geometry geo = getSurfaceGeometry(elm_win_wl_window_get(window_)->surface);
-		Geometry geoFS = window_.getFramespaceGeometry();
-		
-		geo.y += geoFS.y;
-		geo.x += geoFS.x;
-		
-		geo.y += button_.getY();
-		geo.x += button_.getX();
-		
+		queueStep(boost::bind(&ButtonUserMouseInOutTest::test, boost::ref(*this)));
+	}
+
+	void test()
+	{
+		YIELD_UNTIL(rendered_);
+
 		int bw(button_.getWidth());
 		int bh(button_.getHeight());
+		Geometry geo(getSurfaceGeometry(elm_win_wl_window_get(window_)->surface));
+		Geometry fg(window_.getFramespaceGeometry());
+
+		geo.y += fg.y + button_.getY();
+		geo.x += fg.x + button_.getX();
 
 		//NOTE: a button at 0,0 and 10x10 would fill pixels 0-9x0-9
 
 		//top left (vertical)
-		addInOutEventsTest(
+		testMouseInOut(
 			geo.x    , geo.y,
 			geo.x    , geo.y - 1		//1px outside top
 		);
-		
+
 		//top left (horizontal)
-		addInOutEventsTest(
+		testMouseInOut(
 			geo.x    , geo.y,
 			geo.x - 1, geo.y		//1px outside left
 		);
-		
+
 		//top mid
-		addInOutEventsTest(
+		testMouseInOut(
 			bw / 2 + geo.x, geo.y,
 			bw / 2 + geo.x, geo.y - 1	//1px outside top
 		);
-		
+
 		//top right (vertical)
-		addInOutEventsTest(
+		testMouseInOut(
 			bw + geo.x - 1, geo.y,
 			bw + geo.x - 1, geo.y - 1	//1px outside top
 		);
-		
+
 		//top right (horizontal)
-		addInOutEventsTest( 
+		testMouseInOut(
 			bw + geo.x - 1, geo.y,
- 			bw + geo.x    , geo.y		//1px outside right
+			bw + geo.x    , geo.y		//1px outside right
 		);
-		
+
 		//mid left
-		addInOutEventsTest(
+		testMouseInOut(
 			geo.x    , bh / 2 + geo.y,
 			geo.x - 1, bh / 2 + geo.y	//1px outside left
 		);
-		
+
 		//mid right
-		addInOutEventsTest(
+		testMouseInOut(
 			bw + geo.x - 1, bh / 2 + geo.y,
 			bw + geo.x    , bh / 2 + geo.y	//1px outside right
 		);
-		
+
 		//bottom left (vertical)
-		addInOutEventsTest(
+		testMouseInOut(
 			geo.x    , bh + geo.y - 1,
 			geo.x    , bh + geo.y		//1px outside bottom
 		);
-		
+
 		//bottom left (horizontal)
-		addInOutEventsTest(
+		testMouseInOut(
 			geo.x    , bh + geo.y - 1,
 			geo.x - 1, bh + geo.y - 1	//1px outside left
 		);
-		
+
 		//bottom mid
-		addInOutEventsTest(
+		testMouseInOut(
 			bw / 2 + geo.x, bh + geo.y - 1,
 			bw / 2 + geo.x, bh + geo.y	//1px outside bottom
 		);
-		
+
 		//bottom right (vertical)
-		addInOutEventsTest(
+		testMouseInOut(
 			bw + geo.x - 1, bh + geo.y - 1,
 			bw + geo.x - 1, bh + geo.y	//1px outside bottom
 		);
-		
+
 		//bottom right (horizontal)
-		addInOutEventsTest(
+		testMouseInOut(
 			bw + geo.x - 1, bh + geo.y - 1,
 			bw + geo.x    , bh + geo.y - 1	//1px outside right
 		);
 	}
-	
-	void addInOutEventsTest(int xInside, int yInside, int xOutside, int yOutside){
-		//move into button
-		//verify callback
-		//move out of button
-		//verify callback
-		
+
+	void testMouseInOut(int xInside, int yInside, int xOutside, int yOutside)
+	{
+		ASSERT(not mouseIn_);
+
 		//Place pointer inside widget
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::setGlobalPointerPosition, boost::ref(*this),
-				xInside,
-				yInside
-			)
-		);
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::stepUntilCondition,
-				boost::ref(*this),
-				boost::lambda::bind(&UserButtonEventTest::isMouseInButton_, boost::ref(*this))
-			),
-			"checking EVAS_CALLBACK_MOUSE_IN event"
-		);
-		
-		//Place pointer outside widget
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::setGlobalPointerPosition, boost::ref(*this),
-				xOutside,
-				yOutside
-			)
-		);
-		queueStep(
-			boost::bind(
-				&ElmTestHarness::stepUntilCondition,
-				boost::ref(*this),
-				boost::lambda::bind(&UserButtonEventTest::isMouseOutButton_, boost::ref(*this))
-			),
-			"checking EVAS_CALLBACK_MOUSE_OUT event"
-		);
-	}
-	
-	static void onMouseInButton(void *data, Evas*, Evas_Object*, void*)
-	{
-		UserButtonEventTest *test = static_cast<UserButtonEventTest*>(data);
+		setGlobalPointerPosition(xInside, yInside);
 
-		test->isMouseInButton_ = true;
-		test->isMouseOutButton_ = false;
+		YIELD_UNTIL(mouseIn_);
+
+		setGlobalPointerPosition(xOutside, yOutside);
+
+		YIELD_UNTIL(not mouseIn_);
 	}
 
-	static void onMouseOutButton(void *data, Evas*, Evas_Object*, void*)
+	static void onPostRender(void *data, Evas *e, void *info)
 	{
-		UserButtonEventTest *test = static_cast<UserButtonEventTest*>(data);
-		test->isMouseInButton_ = false;
-		test->isMouseOutButton_ = true;
+		evas_event_callback_del(e, EVAS_CALLBACK_RENDER_POST, onPostRender);
+
+		ButtonUserMouseInOutTest *test = static_cast<ButtonUserMouseInOutTest*>(data);
+		test->rendered_ = true;
+		std::cout << "...received post render event" << std::endl;
 	}
-	
-	
+
+	static void onMouseIn(void *data, Evas*, Evas_Object*, void*)
+	{
+		ButtonUserMouseInOutTest *test = static_cast<ButtonUserMouseInOutTest*>(data);
+		test->mouseIn_ = true;
+		std::cout << "...received mouse in event" << std::endl;
+	}
+
+	static void onMouseOut(void *data, Evas*, Evas_Object*, void*)
+	{
+		ButtonUserMouseInOutTest *test = static_cast<ButtonUserMouseInOutTest*>(data);
+		test->mouseIn_ = false;
+		std::cout << "...received mouse out event" << std::endl;
+	}
+
 private:
-	bool		isMouseInButton_;
-	bool		isMouseOutButton_;
 	Window		window_;
 	EvasObject 	button_;
+	bool		mouseIn_;
+	bool		rendered_;
 };
 
 typedef ResizeObjectTest<Button> ButtonResizeTest;
 typedef PositionObjectTest<Button> ButtonPositionTest;
 typedef VisibleObjectTest<Button> ButtonVisibilityTest;
 
-WAYLAND_ELM_HARNESS_TEST_CASE(UserButtonInteractionTest, "Button")
-WAYLAND_ELM_HARNESS_TEST_CASE(UserButtonEventTest, "Button")
+WAYLAND_ELM_HARNESS_TEST_CASE(ButtonUserMouseClickTest, "Button")
+WAYLAND_ELM_HARNESS_TEST_CASE(ButtonUserMouseInOutTest, "Button")
 WAYLAND_ELM_HARNESS_TEST_CASE(ButtonResizeTest, "Button")
 WAYLAND_ELM_HARNESS_TEST_CASE(ButtonPositionTest, "Button")
 WAYLAND_ELM_HARNESS_TEST_CASE(ButtonVisibilityTest, "Button")
