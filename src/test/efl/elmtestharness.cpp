1dfddf6b2b56266a2dc0762e7e9f300e82ad1a29
U. Artie Eoff <ullysses.a.eoff@intel.com>
2013-05-14 09:23:59 -0700

    efl: add some basic harness tests
    Signed-off-by: U. Artie Eoff <ullysses.a.eoff@intel.com>

--- src/test/efl/elmtestharness.cpp
+++ src/test/efl/elmtestharness.cpp
@@ -129,6 +129,85 @@ Eina_Bool ElmTestHarness::doStep(void* data, int, void*)
 	return ECORE_CALLBACK_DONE;
 }
 
+class SimpleHarnessTest : public ElmTestHarness
+{
+public:
+	SimpleHarnessTest()
+		: ElmTestHarness::ElmTestHarness()
+		, nsetup_(0)
+		, nsteps_(0)
+		, nteardown_(0)
+	{
+		return;
+	}
+
+	~SimpleHarnessTest()
+	{
+		ASSERT(1 == nsetup_);
+		ASSERT(50 == nsteps_);
+		ASSERT(1 == nteardown_);
+	}
+
+	void setup()
+	{
+		++nsetup_;
+		for (unsigned i(0); i < 50; ++i) {
+			queueStep(boost::bind(&SimpleHarnessTest::step, boost::ref(*this)));
+		}
+	}
+
+	void step()
+	{
+		++nsteps_;
+	}
+
+	void teardown()
+	{
+		++nteardown_;
+	}
+
+private:
+	unsigned nsetup_;
+	unsigned nsteps_;
+	unsigned nteardown_;
+};
+
+class PointerInterfaceTest : public ElmTestHarness
+{
+public:
+	PointerInterfaceTest()
+		: ElmTestHarness::ElmTestHarness()
+	{
+		return;
+	}
+
+	void setup()
+	{
+		ecore_wl_init(NULL);
+		queueStep(boost::bind(&PointerInterfaceTest::test, boost::ref(*this)));
+	}
+
+	void test()
+	{
+		for (unsigned x(0), y(0); x < 300; x += 51, y += 37) {
+			setGlobalPointerPosition(x, y);
+			Position p(getGlobalPointerPosition());
+			ASSERT(p.x == x and p.y == y);
+			expectGlobalPointerPosition(x, y);
+
+			p.x += 1;
+			p.y += 1;
+			setGlobalPointerPosition(p);
+			Position np(getGlobalPointerPosition());
+			ASSERT(np.x == p.x and np.y == p.y);
+			expectGlobalPointerPosition(p);
+		}
+	}
+};
+
+WFITS_EFL_HARNESS_TEST_CASE(SimpleHarnessTest)
+WFITS_EFL_HARNESS_TEST_CASE(PointerInterfaceTest)
+
 } // namespace efl
 } // namespace test
 } // namespace wfits
